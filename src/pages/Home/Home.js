ab525ff46b56cf92dfdacb5148793047de8facf8
test <jl@Joeys-MacBook-Pro-3.local>
2023-04-11 16:48:52 -0400

    added axios and linked home page to server

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
+import axios from 'axios';
 import Video from '../../components/Video/Video';
 import './Home.css';
 
 const Home = () => {
 
+    const [data, setData] = useState({link:"https://www.youtube.com/embed/NpEaa2P7qZI", name:"PLACE HOLDER"});
+    
+    useEffect(() =>{
+        async function fetchData(){
+            const res = await axios.get("http://localhost:8000/home");
+            setData(res.data);
+        }
+        fetchData();
+    }, []);
+
+    console.log(data);
+
     return (
         <div className='page_padding'>
             <div className='home_title'>Latest Work</div>
-            <Video link='https://www.youtube.com/embed/pJMBOc4qrXE' size='normal' />
-            <div className='home_name'>Manchester: The Meet</div>
+            <Video link={data.link} size='normal' />
+            <div className='home_name'>{data.name}</div>
         </div>
     );
 }
