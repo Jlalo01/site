cc47a5cc1d2791b67c77f186bf44234b3f4e8846
test <jl@Joeys-MacBook-Pro-3.local>
2023-04-11 20:04:28 -0400

    server compatibility Ashort

--- src/pages/Shorts/AShort.js
+++ src/pages/Shorts/AShort.js
@@ -1,29 +1,35 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState} from "react";
+import axios from 'axios';
 import Video from '../../components/Video/Video';
-import data from './shorts.json';
+//import data from './shorts.json';
 import './AShort.css';
 
 const AShort = props => {
 
-    const navigate = useNavigate();
+    const [id, setID] = useState("0")
+    const [data, setData] = useState({"0":{name:""}});
 
-    if (data[props.id] === undefined) {
-        navigate('/shorts');
-    }
+    useEffect(() => {
+        async function fetchData(){
+            const res = await axios.get("http://localhost:8000/shorts");
+            setData(res.data);
+            setID(props.id);
+        }
+        fetchData();
+    }, []);
 
-    
+
     return(
         <div className="page_padding">
-            <div className="title_ashort">{data[props.id].name}</div>
-            <Video link={data[props.id].link} size='normal' />
-            <div className="synopsis_ashort">{data[props.id].synopsis}</div>
-            <div className="info_ashort">Director: {data[props.id].director}</div>
-            <div className="info_ashort">Writer: {data[props.id].writer}</div>
-            <div className="info_ashort">Camera: {data[props.id].camera}</div>
-            <div className="info_ashort">Editor: {data[props.id].editor}</div>
-            <div className="info_ashort">Audio: {data[props.id].audio}</div>
-            <div className="info_ashort">Cast: {data[props.id].cast}</div>
+            <div className="title_ashort">{data[id].name}</div>
+            <Video link={data[id].link} size='normal' />
+            <div className="synopsis_ashort">{data[id].synopsis}</div>
+            <div className="info_ashort">Director: {data[id].director}</div>
+            <div className="info_ashort">Writer: {data[id].writer}</div>
+            <div className="info_ashort">Camera: {data[id].camera}</div>
+            <div className="info_ashort">Editor: {data[id].editor}</div>
+            <div className="info_ashort">Audio: {data[id].audio}</div>
+            <div className="info_ashort">Cast: {data[id].cast}</div>
         </div>
     );
 }
