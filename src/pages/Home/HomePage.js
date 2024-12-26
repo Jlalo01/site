c8616e0fdfd3b658587afebb7ca6451a5cf83b10
Jlalo01 <110356514+Jlalo01@users.noreply.github.com>
2024-12-26 17:28:55 -0500

    reroute test1

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Footer from '../../components/Footer/Footer';
 import './HomePage.css';
 
 const HomePage = () =>{
-    
+    const navigate = useNavigate();
+    navigate('mirumproductions.com', { replace: true });
     return (
         <div className='home_page'>
-            <div>hello there</div>
             <Footer />
         </div>
     );
